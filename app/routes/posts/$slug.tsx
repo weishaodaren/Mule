577dd0860b2f44a6d3ca31fd67ac86e963c3cf8f
weishaodaren <weishaodaren@gmail.com>
2022-05-16 16:23:22 +0800

    invariant

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -1,17 +1,28 @@
 import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import invariant from "tiny-invariant";
+
+import type { Post } from "~/models/post.server";
+import { getPost } from "~/models/post.server";
+
+type LoaderData = {
+  post: Post;
+};
 
 export const loader: LoaderFunction = async ({ params }) => {
-  return json({ slug: params.slug });
+  invariant(params.slug, "params.slug is required");
+  const post = await getPost(params.slug);
+  invariant(post, `Post not found ${params.slug}`);
+  return json<LoaderData>({ post });
 };
 
 export default function PostSlug() {
-  const { slug } = useLoaderData();
+  const { post } = useLoaderData() as LoaderData;
   return (
     <main className="max-2-4xl mx-auto">
       <h1 className="my-6 border-b-2 text-center text-3xl">
-        Some Post: {slug}
+        Some Post: {post.title}
       </h1>
     </main>
   );
