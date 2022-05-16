e245f615c1879c240bc22f4b1fd38ad1a33ca849
weishaodaren <weishaodaren@gmail.com>
2022-05-16 15:44:49 +0800

    db

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,15 +1,8 @@
+import { prisma } from "~/db.server";
+
 type Post = {
   slug: string;
   title: string;
 };
 
-export const getPosts = async (): Promise<Post[]> => [
-  {
-    slug: "my-first-post",
-    title: "My First Post",
-  },
-  {
-    slug: "90s-mixtape",
-    title: "A Mixtape I Made Just For You",
-  },
-];
+export const getPosts = async (): Promise<Post[]> => prisma.post.findMany();
