68ac6511609b07970ee1a0fb92be5be49716eabe
weishaodaren <weishaodaren@gmail.com>
2022-05-16 14:35:00 +0800

    Link

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,5 +1,10 @@
 import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
+
+interface Post {
+  slug: string;
+  title: string;
+}
 
 export const loader = async () => {
   return json({
@@ -12,17 +17,26 @@ export const loader = async () => {
         slug: "90s-mixtape",
         title: "A Mixtape I Made Just For You",
       },
-    ],
+    ] as Post[],
   });
 };
 
 export default function Posts() {
-  const { posts } = useLoaderData();
+  const { posts } = useLoaderData() as { posts: Post[] };
   console.log(posts);
 
   return (
     <main>
       <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
